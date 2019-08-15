5f62af972cf7a271980f3a06785f4f5c383ca2b6
awaylee <969603910@qq.com>
2019-08-15 19:00:02 +0800

    city-router

---
+++ src/pages/city/city.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="city">
+      <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/header'
+export default {
+  name: 'city',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/city/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cityHeader'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  .header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-back
+      position absolute
+      width .64rem
+      text-align center
+      font-size .4rem
+      top 0
+      left 0
+      color #fff
+
+</style>
