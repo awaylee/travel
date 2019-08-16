4a54683a3dc45c3f22608faefb3d4ef32d7a53d4
awaylee <969603910@qq.com>
2019-08-16 14:57:18 +0800

    search finish

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="city">
       <city-header></city-header>
-      <city-search></city-search>
+      <city-search :cities="cities"></city-search>
       <city-list
         :cities="cities"
         :hot="hotCities"
--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'search'
+  name: 'search',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      })
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -20,10 +69,24 @@ export default {
       box-sizing border-box
       width 100%
       height .6rem
-      padding 0 .1rem
+      padding .1rem
       line-height .6rem
       text-align center
       border-radius .06rem
       color #666
+  .search-content
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    z-index 1
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background #fff
 
 </style>
