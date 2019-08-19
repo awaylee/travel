3c7817cbe9781f12953234faf52238ceca627ae5
awaylee <969603910@qq.com>
2019-08-19 13:37:21 +0800

    detail-list

---
+++ src/pages/detail/components/list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="detailList">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detailList'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-list></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
+import DetailList from './components/list'
 export default {
   name: 'detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailList
   }
 }
 </script>
