a7b7f46bd5777acc6ecfde1c4816c1a9e0a766bb
awaylee <969603910@qq.com>
2019-08-19 10:36:53 +0800

    banner finish

---
+++ src/common/gallary/gallary.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+   <div class="wrapper">
+     <swiper :options="swiperOption">
+       <!-- slides -->
+       <swiper-slide v-for="(item, index) in imgs" :key="index">
+         <img class="gallary-img" :src="item" alt="">
+       </swiper-slide>
+       <!-- Optional controls -->
+       <div class="swiper-pagination"  slot="pagination"></div>
+     </swiper>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gallary',
+  props: {
+    imgs: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.container >>> .swiper-container
+  overflow inherit
+.container
+  display flex
+  flex-direction column
+  justify-content center
+  position fixed
+  top 0
+  right 0
+  left 0
+  bottom 0
+  background #000
+  z-index 99
+  .wrapper
+    width 100%
+    height 0
+    padding-bottom 100%
+    .gallary-img
+      width 100%
+    .swiper-pagination
+      color #fff
+      bottom -1rem
+
+</style>
--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="https://img1.qunarzz.com/p/tts7/1605/c1/ad42f60508d31cf7.jpg_r_640x420x90_93655b91.jpg" alt="">
-    <div class="banner-info">
-      <div class="banner-title">产品编号 3140230995</div>
-      <div class="banner-number"><span class="iconfont banner-icon">&#xe604;</span>39</div>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="https://img1.qunarzz.com/p/tts7/1605/c1/ad42f60508d31cf7.jpg_r_640x420x90_93655b91.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">产品编号 3140230995</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe604;</span>39</div>
+      </div>
     </div>
+    <common-gallary
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/gallary'
 export default {
-  name: 'banner'
+  name: 'banner',
+  data () {
+    return {
+      imgs: ['https://img1.qunarzz.com/p/tts7/1605/c1/ad42f60508d31cf7.jpg_r_640x420x90_93655b91.jpg',
+        'https://img1.qunarzz.com/p/tts5/1511/74/352a9439dabfe6f7.jpg_r_640x420x90_235e71eb.jpg'],
+      showGallary: false
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
